cdce1b16e62abe3441bdf1cc6d5f044fabf75d6c
Yawning Angel <yawning@schwanenlied.me>
2023-06-04 20:04:58 +0900

    key: Add basic s11n tests

--- key_test.go
+++ key_test.go
@@ -1,17 +1,20 @@
 package did
 
 import (
+	"bytes"
 	"crypto/rand"
 	"testing"
 )
 
+var allKeyTypes = []string{
+	KeyTypeSecp256k1,
+	KeyTypeP256,
+	KeyTypeEd25519,
+}
+
 func TestKey(t *testing.T) {
 	t.Run("Integration", func(t *testing.T) {
-		for _, keyType := range []string{
-			KeyTypeSecp256k1,
-			KeyTypeP256,
-			KeyTypeEd25519,
-		} {
+		for _, keyType := range allKeyTypes {
 			t.Run(keyType, func(t *testing.T) {
 				sk, err := GeneratePrivKey(rand.Reader, keyType)
 				if err != nil {
@@ -35,6 +38,47 @@ func TestKey(t *testing.T) {
 
 				if err := pk.Verify(msg, sig); err != nil {
 					t.Fatal(err)
+				}
+			})
+		}
+	})
+
+	t.Run("s11n", func(t *testing.T) {
+		for _, keyType := range allKeyTypes {
+			t.Run(keyType, func(t *testing.T) {
+				sk, err := GeneratePrivKey(rand.Reader, keyType)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				skBytes, _ := sk.RawBytes()
+				_ = skBytes // At some point, test round-tripping this.
+				pk := sk.Public()
+
+				// Roundtrip Multibase encoding.
+				pkStr := pk.MultibaseString()
+
+				vm := VerificationMethod{
+					Type:               keyType,
+					PublicKeyMultibase: &pkStr,
+				}
+				pk2, err := KeyFromMultibase(vm)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if pk.Type != pk2.Type {
+					t.Fatalf("public key type did not round-trip: got %s, expected %s", pk2.Type, pk.Type)
+				}
+
+				pkB, pk2B := pk.Raw.([]byte), pk2.Raw.([]byte)
+				if !bytes.Equal(pkB, pk2B) {
+					t.Fatalf("public key raw did not round-trip: got %x, expected %x", pk2B, pkB)
+				}
+
+				pkDID, pk2DID := pk.DID(), pk2.DID()
+				if pkDID != pk2DID {
+					t.Fatalf("public key DID did not round-trip: got %s, expected %s", pk2DID, pkDID)
 				}
 			})
 		}
