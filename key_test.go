e5558e0876de4ac2d91983178528ac5420f857d2
Yawning Angel <yawning@schwanenlied.me>
2023-06-04 18:24:10 +0900

    key: Add basic integration tests

--- key_test.go
+++ key_test.go
@@ -1,34 +1,42 @@
 package did
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"crypto/rand"
-	"fmt"
 	"testing"
 )
 
-func TestKeySignVerify(t *testing.T) {
-	k, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		t.Fatal(err)
-	}
+func TestKey(t *testing.T) {
+	t.Run("Integration", func(t *testing.T) {
+		for _, keyType := range []string{
+			KeyTypeSecp256k1,
+			KeyTypeP256,
+			KeyTypeEd25519,
+		} {
+			t.Run(keyType, func(t *testing.T) {
+				sk, err := GeneratePrivKey(rand.Reader, keyType)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if sk.Type != keyType {
+					t.Fatalf("secret key type mismatch: got %s, expected %s", sk.Type, keyType)
+				}
 
-	sk := &PrivKey{
-		Type: KeyTypeP256,
-		Raw:  k,
-	}
+				pk := sk.Public()
+				if pk.Type != keyType {
+					t.Fatalf("public key type mismatch: got %s, expected %s", pk.Type, keyType)
+				}
 
-	msg := []byte("foo bar beep boop bop")
+				msg := []byte("foo bar beep boop bop")
 
-	sig, err := sk.Sign(msg)
-	if err != nil {
-		t.Fatal(err)
-	}
+				sig, err := sk.Sign(msg)
+				if err != nil {
+					t.Fatal(err)
+				}
 
-	fmt.Println(sig)
-
-	if err := sk.Public().Verify(msg, sig); err != nil {
-		t.Fatal(err)
-	}
+				if err := pk.Verify(msg, sig); err != nil {
+					t.Fatal(err)
+				}
+			})
+		}
+	})
 }
